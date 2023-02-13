fa9478a81bc3c99cbff7e3f8ad8589e28edcb1a2
kishan980 <kishany980@gmail.com>
2023-02-13 16:02:38 +0530

    update

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(
     cors({
         origin:process.env.FRONTEND_URL,
         credentials:true,
-        methods:["GET","POST","PUT","DELETE"]
+        methods:["GET","POST","PUT","DELETE"] 
     })
 )
 
