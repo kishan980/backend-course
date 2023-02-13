cb1999aa404a7fd41aa1793efac144f261264760
kishan980 <kishany980@gmail.com>
2023-02-13 16:08:15 +0530

    add

--- app.js
+++ app.js
@@ -30,7 +30,6 @@ app.use(
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
  });
 app.use("/api/v2", payment)
